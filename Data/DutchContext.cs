3e137e9a759fee24ee9d2f523ba7ae8b04f6b759
isabellesavannah <isabellet.208@gmail.com>
2021-07-21 10:46:04 -0600

    Added connection strings and config.json

--- Data/DutchContext.cs
+++ Data/DutchContext.cs
@@ -11,5 +11,12 @@ namespace DutchTreat.Data
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer();
+        }
     }
 }
