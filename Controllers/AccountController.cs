d4b29cd6133774de92eb949781fae2462dfb087c
isabellesavannah <isabellet.208@gmail.com>
2021-07-28 12:18:31 -0600

    building out signin view. Issue with database so errors out when trying to login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.Data.Entities;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -10,13 +13,16 @@ namespace DutchTreat.Controllers
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<StoreUser> _signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger,
+                                 SignInManager<StoreUser> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
-        //----------------------------------------------------------Log in
+        //----------------------------------------------------------If loged in redirect
         public IActionResult Login()
         {
             if (this.User.Identity.IsAuthenticated)
@@ -24,6 +30,34 @@ namespace DutchTreat.Controllers
                 return RedirectToAction("Index", "App");
             }
 
+            return View();
+        }
+
+        //----------------------------------------------------------Log in
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(
+                    model.Username,
+                    model.Password,
+                    model.RememberMe,
+                    false);
+
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    else
+                    {
+                        return RedirectToAction("Shop", "App");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Failed to login");
             return View();
         }
     }
